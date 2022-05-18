446e83c4c3e4e7266c896b237aeee9eb6436d7ea
Pedro Mandú <pedromandu7@gmail.com>
2022-05-18 20:32:09 -0300

    primeira aula

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import { Widget } from "./components/widget"
+export function App(){
+  return <Widget/>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand:{
+          500: '#8257E5'
+        },
+        brand_hover:{
+          500: '#996DFF'
+        },
+        text_on_brand_color: {
+          500: '#FFFFFF'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
